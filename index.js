e6f232554f848102f292fc08625189d29456ded5
Seokyunghwan <dhgh8875@icloud.com>
2025-02-11 16:14:00 +0900

    keep work

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ lenis.raf(time * 600);
 gsap.ticker.lagSmoothing(0);
 
 
-
+// h-hero gsap
 const hero = gsap.timeline({
     scrollTrigger:{
         trigger: $('.h-hero__sticky-wrapper'),
@@ -16,13 +16,8 @@ const hero = gsap.timeline({
         scrub:1,
     }
 });
-hero.to('.cloud-right_img',{
-    y:-50
-})
-hero.to('.cloud-left_img',{
-    y:100
-},"<");
-
+hero.to('.cloud-right_img',{ y:-50 })
+hero.to('.cloud-left_img',{ y:100 },"<");
 
 const hero1 = gsap.timeline({
     scrollTrigger:{
@@ -30,10 +25,50 @@ const hero1 = gsap.timeline({
         start:"50% 50%",
         end: "100% 50%",
         scrub:1,
-        markers:1,
-
     }
 });
-hero1.to('.h-hero__about-mask',{
-    opacity:0.7
+hero1.to('.h-hero__about-mask',{ opacity:0.7 });
+
+
+// h-wave gsap
+gsap.set('.h-wave_img-wrapper .h-wave__image-parent',{
+    y:50
+});
+const wave = gsap.timeline({
+    scrollTrigger:{
+        trigger:$('.h-wave_img-wrapper'),
+        start:"20% 50%",
+        end:"100% 50%",
+        scrub:1,
+    },
+});
+wave.to('.h-wave_img-wrapper .h-wave__image-parent',{
+    y:0
 })
+wave.to('.h-wave__image-parent img:nth-child(9)',{
+    opacity:0
+})
+wave.to('.h-wave__image-parent img:nth-child(8)',{
+    opacity:0
+})
+wave.to('.h-wave__image-parent img:nth-child(7)',{
+    opacity:0
+})
+wave.to('.h-wave__image-parent img:nth-child(6)',{
+    opacity:0
+})
+wave.to('.h-wave__image-parent img:nth-child(5)',{
+    opacity:0
+})
+wave.to('.h-wave__image-parent img:nth-child(4)',{
+    opacity:0
+})
+wave.to('.h-wave__image-parent img:nth-child(3)',{
+    opacity:0
+})
+wave.to('.h-wave__image-parent img:nth-child(2)',{
+    opacity:0
+})
+wave.to('.h-wave__image-parent img:nth-child(1)',{
+    opacity:0
+});
